973602aefa6b4777c159b4eb4474df4cd911e8bf
nithya-6126 <nithya2621r@gmail.com>
2026-07-15 14:34:29 +0530

    started working on backend

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+//import productRoutes from "./routes/products.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/products", productRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
